0a6e10e3188b5b067393050efb01393c41c4e20e
Riukan <rk850323@naver.com>
2021-03-22 00:43:23 +0900

    밀린복습기능구현완료

--- 데이터 수정.py
+++ 데이터 수정.py
@@ -1,8 +1,22 @@
 import json
+import os
+import time
 input("데이터를 수정합니다 작업 전 백업 해놓으세요.")
 with open("review.txt","r") as f:
     logs = json.load(f)
-logs["빅데이터"]["3.17"] = [["41","51"]]
-# with open 안에서 위 코드 넣으면 왜 날라가는거임?
-with open("review.txt","w") as f:
-    json.dump(logs,f)
+
+##while True:
+##    os.system("cls")
+##    a = input("과목,날짜(3.1),범위(-) : ")
+##    a = a.split(",")
+##    b = a[2].split("-")
+##    try:
+##        logs[a[0]][a[1]].append(b)
+##    except KeyError:
+##        logs[a[0]][a[1]] = b
+### with open 안에서 위 코드 넣으면 왜 날라가는거임?
+##    with open("review.txt","w") as f:
+##        json.dump(logs,f)
+##    print("수정 완료")
+##    time.sleep(0.2)    
+    
--- 복습.py
+++ 복습.py
@@ -2,6 +2,8 @@ import time, json
 import random
 import re
 import os
+# 기초 도구가 탄탄해야 효율적으로 코드를 짬. 효율 차이가 스위치 수준으로 수 배 차이 날 때가 많음.
+# 올해만 작동하게 되어있음
 # 디자인, collocation 시험 구현
 # 함수나 변수로 수정 필요한 것 혹은 중복되는 것 빼서 코드 정리 (수정이 쉬움)
 # 복습 여부 기록 (+ 알고 모르는 것 기록해서 따로 관리) , 이어하기,새로하기 
@@ -28,7 +30,9 @@ import os
 # 암기 내용들도 입력하기
 # At this hour ?
 # informative (유익하다고)
-review_list = (86400*30, 86400*14, 86400*7, 86400*4, 86400 )
+
+# 암기 내용 추가도 괜찮을 듯.
+review_list = (86400*30, 86400*14, 86400*7, 86400*4, 86400)
 def save(logs):
     with open("review.txt","w") as f:
             json.dump(logs,f)
@@ -42,7 +46,12 @@ def three_stack(li_st):
                 print("\n"+"\t".join([f"({t+i}) "+ z for i,z in enumerate(li_st[t:t+3])]))
                 t += 3
     
-
+def on_press(key):
+    
+    
+    
+    return key
+    
 def start():
     global logs
     while True:
@@ -57,6 +66,11 @@ def start():
 
 
 어떤 것을 복습하시겠습니까? """)
+        
+
+
+        
+            
 
         if intro == "a" or intro == "A":
             new = input("""
@@ -127,92 +141,265 @@ def delayed_cal(title):
     y = n-86400
     yesterday = f"{lt(y)[1]}.{lt(y)[2]}"
     today = f"{lt()[1]}.{lt()[2]}"
-    initialize_data = {}
     review_terms = (1,4,7,14,30)
     review_dates = logs[title]["review"].keys()
     data_days = {}
     
     
-    if today in review_dates: # 오늘 복습 했으면 1회 넣기 
+    if today in review_dates: # 오늘 복습 했으면 1회 넣기
+        # 각각 학습날짜 와 현재날짜 차이 계산해서, 복습 필수 횟수  넣기
         for i in logs[title].keys():
             if i == "review" or i == "delayed": # 키목록에 날짜 마지막에 review 키가 있어서, 제외
                 continue
+            data_days[i] = []
             day_diff = (n-time.mktime(time.strptime(f"{lt()[0]}."+i,"%Y.%m.%d")))//86400
             for l,t in enumerate(review_terms):
                 l += 1
                 if t == day_diff: # 아래랑 여기만 차이 있음
-                    data_days[i] = l # 복습 했으면, 복습 검사 할때 오늘꺼 빼주게 됨.
+                    data_days[i].append(l)
+                    if l == 5: # 앞으로 예정 복습까지 남은 일수 계산
+                        data_days[i].append(None)
+                    else:
+                        data_days[i].append(review_terms[l+1]-t)# 복습 했으면, 복습 검사 할때 오늘꺼 빼주게 됨.
                     break
                 elif t > day_diff:
-                    data_days[i] = l-1 
+                    data_days[i].append(l-1)
+                    # l이 5라도, 똑같은 결과
+                    data_days[i].append(review_terms[l]-day_diff)
                     break
-                data_days[i] = 5
-                break
-            
-            for k in range(data_days[i]):
+                else:
+                    if l==5:
+                        data_days[i].append(5)
+                        data_days[i].append(None)
+                    continue
+                
+            # 복습한 횟수 빼내기
+            count = 0 
+            for k in range(data_days[i][0]):
                 z = review_list[-k-1]
-                tmp_day = time.localtime(time.mktime(time.strptime(f"{lt()[0]}."+i,"%Y.%m.%d"))+z) 
+                tmp_time = time.mktime(time.strptime(f"{lt()[0]}."+i,"%Y.%m.%d"))+z
+                
+                tmp_day = time.localtime(tmp_time) 
             # 날짜 변환 함수 만들기
                 day_string = f"{tmp_day[1]}.{tmp_day[2]}"
+                 # 첫번째 걸린 날짜 가 최근 복습 일자라서 
                 if day_string in review_dates:
-                    data_days[i] -= 1
-                else:
-                    initialize_data.add(day_string)
+                    data_days[i][0] -= 1
+                    if count == 0:
+                        data_days[i].append(day_string)
+                    count += 1
+            if count == 0:
+                data_days[i].append(None)
+               
                     
                     
     else:   # 안했으면 1회 빼기
         for i in logs[title].keys():
             if i == "review" or i == "delayed": 
                 continue
-            
+            data_days[i] = []
             day_diff = (n-time.mktime(time.strptime(f"{lt()[0]}."+i,"%Y.%m.%d")))//86400
             for l,t in enumerate(review_terms):
                 l += 1
                 if t >= day_diff:
                     
-                    data_days[i] = l-1
+                    data_days[i].append(l-1)
+                    data_days[i].append(review_terms[l]-day_diff)
                     break
-                data_days[i] = 5
-                break
-            
-            for k in range(data_days[i]):
+                else:
+                    if l==5:
+                        data_days[i].append(5)
+                        data_days[i].append(None)
+                    continue
+                
+            count = 0
+            for k in range(data_days[i][0]):
                 z = review_list[-k-1]
-                tmp_day = time.localtime(time.mktime(time.strptime(f"{lt()[0]}."+i,"%Y.%m.%d"))-z) 
+                tmp_time = time.mktime(time.strptime(f"{lt()[0]}."+i,"%Y.%m.%d"))+z
+                
+                tmp_day = time.localtime(tmp_time) 
             # 날짜 변환 함수 만들기
                 day_string = f"{tmp_day[1]}.{tmp_day[2]}"
-                if day_string in data_days:
-                    data_days[i] -= 1
-                else:
-                    initialize_data.add(day_string)
-                    
-    # 남은 횟수가 5회이면 오늘 공부할걸로 넣어서 다시 복습 시킴 (한번도 복습 안해서)
-    if 5 in data_days.values():
-        for key,value in data_days.items(): # 5번 이면 
-            if value == 5:
-                try:
-                        logs[title][today]
-                except KeyError:
-                        logs[title][today] = []
-                for i in logs[title][key]:
-                    for index,z in enumerate(i):
-                        i[index] = z+f" *{key}"
-                    logs[title][today].append(i)
-                del data_days[key]
-                del logs[title][key]
-    for i in initialize_data:
-        logs[title]["review"][i] = True
-    try:
-        for key,value in data_days.items():
                 
-            logs[title]["delayed"][key] += data_days[key]
-    except KeyError:
-        logs[title]["delayed"][key] = data_days[key]
+                if day_string in data_days:
+                    data_days[i][0] -= 1
+                    if count == 0:
+                        data_days[i].append(day_string)
+                    count += 1
+            if count == 0:
+                data_days[i].append(None)
+
+        
+    
+    for key,value in data_days.items():
+            
+        if value[0] != 0:
+            logs[title]["delayed"][key] = value
+
+    # 5회 밀린 회차는, 당일 학습으로 넣기(5번 안한건 오늘 한걸로 다시 넣기),               
+    delete = []
+    for key,value in logs[title]["delayed"].items():
+        if key == "did":
+            continue
+        try:
+            delayed_count = value[0] - logs[title]["delayed"]["did"][key][0]
+        except KeyError:
+            delayed_count = value[0]# 5번 이면 
+        if delayed_count == 5:
+            if not logs[title].get(today):
+                logs[title][today] = []
+            for i in logs[title][key]:
+                for index,z in enumerate(i):
+                    i[index] = z+f" *{key}"
+                logs[title][today].append(i)
+                delete.append(key)
+                del logs[title][key]
+    for i in delete:
+        del logs[title]["delayed"][i]
+
     with open("review.txt","w") as f:
         json.dump(logs,f)
-        
-        # 51-61
+def delayed_play(title):
+    delayed_cal(title)
+    recommend = []
+    delay_dict = logs[title]["delayed"] # {학습날짜:[밀린 횟수,예정 복습까지 남은 일수,최근 복습일],did:{학습날짜:[복습횟수,최근 복습일]}}
+    try:
+        delay_did = logs[title]["delayed"]["did"]
+    except KeyError:
+        delay_did = {}
+    key_list = []
+    for i in delay_dict.keys():
+        try:
+            if delay_dict[i][0] == delay_did[i][0] or i == "did":
+                continue
+            else:
+                key_list.append(i)
+        except KeyError:
+            if i == "did":
+                continue
+            key_list.append(i)
+                
+    delay_dict_keys = sorted(key_list)
+    if key_list:
     
+        for index, key in enumerate(delay_dict_keys):
             
+            numbers_1, next_review, recent_review_1 = delay_dict[key][0],delay_dict[key][1],(delay_dict[key][2] if delay_dict[key][2] else None)
+            try:
+                numbers_2, recent_review_2 = delay_did[key][0], delay_did[key][1]
+                
+            except KeyError:
+                
+                numbers_2, recent_review_2 = 0,None
+            
+            if not recent_review_1 or recent_review_2:
+                time_diff = None
+            elif recent_review_1 and recent_review_2:
+                time_diff = (time.time() - time.mktime(time.strptime(f"{time.localtime()[0]}."+ (recent_review_1 if recent_review_1>=recent_review_2 else recent_review_2) ,"%Y.%m.%d")))//86400
+            else:
+                time_diff = (time.time() - time.mktime(time.strptime(f"{time.localtime()[0]}."+ (recent_review_1 if recent_review_1 else recent_review_2) ,"%Y.%m.%d")))//86400 # 올해만 작동하게 되어있음
+            print (f"\n\n({index}) {key} 일자 :: {str(time_diff) + ' 일 전 복습' if time_diff else '이전 복습 없음' } | {numbers_1-numbers_2} 회 미복습 | {str(int(next_review))+'일 후 복습 예정' if next_review else '예정 없음'}")
+            
+            
+            if (next_review == None or next_review > 3) and (time_diff == None or time_diff > 3):
+                
+                recommend.append(index)
+        print("\n\n추천 복습 : ", " , ".join([f"({i})"for i in recommend]))
+        while True:
+            choiced_review = input("\n하고자 하는 복습 일자 및 번호를 입력하세요 (b. 이전) : ")
+            if choiced_review == "b" or choiced_review == "":
+                print("취소 되었습니다")
+                time.sleep(0.2)
+                break
+            try:
+                key_choiced = delay_dict_keys[int(choiced_review)]
+                contents = logs[title][key_choiced]
+                
+            except ValueError:
+                try:
+                    key_choiced = choiced_review
+                    contents = logs[title][key_choiced]
+                    
+                except KeyError:
+                    print("날짜를 잘못 입력하셨습니다.")
+                    time.sleep(0.2)
+                    continue
+            except IndexError:
+                print("번호가 범위를 초과하였습니다.")
+                time.sleep(0.2)
+                continue
+        
+        
+            if title == "영어":
+                word_list = []
+                for i in contents:
+                        
+                        word_list.append(i)
+                print("\n복습을 시작합니다.\n\n 복습은 랜덤하게 제공됩니다.(b. 취소)")
+                while word_list: 
+                    eng_kor_choice = random.randrange(0,2)
+                    word_choiced = random.choice(word_list) # 랜덤한 문장과 뜻 순서, input으로 물어보기
+                    first = input("\n"+word_choiced[eng_kor_choice])
+                    if first == "b":
+                       confirm = "n" 
+                       break
+                    else:
+                        
+                        second = input(word_choiced[eng_kor_choice == 0])
+                        if second == "b":
+                            confirm = "n" 
+                            break
+                        else:
+                            del word_list[word_list.index(word_choiced)]
+                try:
+                    if confirm:
+                        pass
+                except NameError:
+                    confirm = input("\n복습 체크 하시겠습니까? Y/n | ")
+                    
+                        
+            else:
+                word_list = []
+                            
+                
+                    
+                        
+                print(contents)
+                for z in contents:
+                            
+                            word_list.append(["-".join(z)]) 
+                if word_list:
+                    print("\n복습할 진도들입니다. ")
+                    print("\n"+"\n".join(["\n".join(i) for i in word_list]))
+                
+                confirm = input("\n복습 체크 하시겠습니까? Y/n | ")
+            if confirm == "Y" or confirm == "y" or confirm == "":
+                try:
+                    delay_did[key_choiced][0] += 1
+                    delay_did[key_choiced][1] = f"{time.localtime()[1]}.{time.localtime()[2]}"
+                    if delay_dict[key_choiced][0] == delay_did[key_choiced][0] and delay_dict[key_choiced][1] == None: # 소멸 되는 조건, 횟수를 채울 때 예정 복습일이 없어야 함.안그러면 추후 밀렸을 시 최근 복습 일자를 잃게 됨
+                        del logs[title]["delayed"][key_choiced], logs[title]["delayed"]["did"][key_choiced]
+                        del delay_did[key_choiced], delay_dict[key_choiced]
+                except KeyError:
+                    delay_did[key_choiced] = []
+                    delay_did[key_choiced].append(1) 
+                    delay_did[key_choiced].append(f"{time.localtime()[1]}.{time.localtime()[2]}")
+                    if delay_dict[key_choiced][0] == delay_did[key_choiced][0] and delay_dict[key_choiced][1] == None:
+                        del logs[title]["delayed"][key_choiced], logs[title]["delayed"]["did"][key_choiced]
+                        del delay_did[key_choiced], delay_dict[key_choiced]
+                
+                logs[title]["delayed"]["did"] = delay_did
+                print("체크 완료")
+                time.sleep(0.2)
+                break
+            else:
+                print("취소되었습니다")
+                time.sleep(0.2)
+                break
+    else:
+        print("\n밀린 복습이 없습니다")
+        time.sleep(0.2)
+        
             
         
     
@@ -238,8 +425,9 @@ def delayed_cal(title):
             
 def word():
     delayed = []
-    delayed_cal("영어")
+    
     while True:
+        
         os.system('cls')
 ##        try:
             
@@ -249,15 +437,14 @@ def word():
             check = "오늘 복습 완료" if logs["영어"]["review"][f"{l(n)[1]}.{l(n)[2]}"] else ""
         except KeyError:
             check = "복습을 아직 안했습니다."
-        word_menu = input(f"\n영어 복습 입니다.\n\n{check}\n\n1. 복습하기  2. 추가하기 3. 오늘 기록보기 4. 전체 기록보기 b. 이전 ")
+        word_menu = input(f"\n영어 복습 입니다.\n\n{check}\n\n1. 복습하기  2. 추가하기 3. 오늘 기록보기 4. 전체 기록보기 | b. 이전 | ")
         if word_menu == "1":
             
             
             review_choice = input("\n1. 밀린 복습하기 2. 복습하기 ")
             if review_choice == "1":
-                
-                input("돌아가려면 아무 키나 입력하시오")
-
+                delayed_play("영어")
+        
             elif review_choice == "2":
             
             
@@ -271,6 +458,7 @@ def word():
                             word_list.append(i) # 복습으로 뽑아낼 word_list 만들기
                     except KeyError: # 존재하는 날짜만 뽑아오기
                         pass
+            
                 
                     
                     
@@ -294,15 +482,17 @@ def word():
                     
                     
                     logs["영어"]["review"][f"{l(n)[1]}.{l(n)[2]}"] = True
-                                
+                    
+                     
                 
                     
                 
                             
-            
+                    break
                 else:
                     print("복습할 것이 없습니다.")
                     time.sleep(0.2)
+            
                 
             
         elif word_menu == "2":
@@ -342,6 +532,8 @@ def word():
                         else:
                             try:
                                 del logs["영어"][date][int(choiced_date)]
+                                if not logs["영어"][date]:
+                                    del logs["영어"][date]
                                 save(logs)
                                 
                                 
@@ -410,6 +602,8 @@ def word():
                                     else:
                                         try:
                                             del logs["영어"][date][int(choiced_date)]
+                                            if not logs["영어"][date]:
+                                                del logs["영어"][date]
                                             save(logs)
                                             
                                         except IndexError:
@@ -453,8 +647,9 @@ def word():
 ##def collocation():
 def anything(menu):
     title = logs['목록'][menu-1]
-    delayed_cal(title)
+    
     while True:
+        
         os.system('cls')
         try:
             n = time.time()
@@ -467,8 +662,7 @@ def anything(menu):
             if word_menu == "1":
                 review_choice = input("\n1. 밀린 복습하기 2. 복습하기 ")
                 if review_choice == "1":
-                    
-                    input("돌아가려면 아무 키나 입력하시오")
+                    delayed_play(title)
 
                 elif review_choice == "2":
                     
@@ -547,6 +741,8 @@ def anything(menu):
                                 else:
                                     try:
                                         del logs[title][date][int(any_choiced_date)]
+                                        if not logs["title"][date]:
+                                            del logs["title"][date]
                                         save(logs)
                                         
                                     except IndexError:
@@ -642,6 +838,8 @@ def anything(menu):
                                 else:
                                     try:
                                         del logs[title][date][int(any_choiced_date)]
+                                        if not logs["title"][date]:
+                                            del logs["title"][date]
                                         save(logs)
                                         
                                     except IndexError:
